255376214d0bdb09a56cf3c21c8c67d740ef4d88
nicolas-brement <nicolas.brement@laplateforme.io>
2022-10-19 23:14:52 +0200

    Ajout des Job

--- job07.py
+++ job07.py
@@ -1,11 +1,11 @@
-for fizzbuzz in range(100):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
+for fizzbuzz in range(100):                         #Je crée ma boucle for ainsi qu'une liste d'une valeur numérique de 100.
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:     #J'indique que lorsqu'un chiffre est un multiple de 3 et de 5 à
+        print("fizzbuzz")                           #alors "fizzbuzz" s'affichera à la place de ce chiffre.
         continue
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
+    elif fizzbuzz % 3 == 0:                         #J'indique que lorsqu'un chiffre est un multiple de  3
+        print("fizz")                               #alors "fizz" s'affichera à la place de ce chiffre.
         continue
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
+    elif fizzbuzz % 5 == 0:                         ##J'indique que lorsqu'un chiffre est un multiple de 5
+        print("buzz")                               #alors "buzz" s'affichera à la place de ce chiffre.
         continue
     print(fizzbuzz)
--- job19.py
+++ job19.py
@@ -1,14 +1,14 @@
-def myFunction(*args):
-    args = list(args)
-    r=[]
+def myFunction(*args):                      #Je créer "myFunction" suivi de mon paramètre.
+    args = list(args)                       #Je créer une première liste nommé "args".     
+    r=[]                                    #Je créer une deuxième liste nommé "r".
 
-    while args:
+    while args:                             #Je créer une boucle while afin de pouvoir déplacer des valeurs d'un fichier à un autre par ordre croissant.
         mini=args[0]
-        for x in args:
+        for x in args:                      
             if x < mini:
                 mini=x
-        args.remove(mini)
-        r.append(mini)
+        args.remove(mini)                   #.remove permet de supprimer la première valeur.
+        r.append(mini)                      #.append permet d'ajouter un élément à la fin de la liste.
     return r
 
 print(myFunction(17,7,91,12,1,13,4,12,18))
