94169ac038548609c269d1f9f7250fcfefeb2538
nicolas-brement <nicolas.brement@laplateforme.io>
2022-10-19 23:22:05 +0200

    Ajout des Job

--- job21.py
+++ job21.py
@@ -3,10 +3,10 @@ def mySort(*args):
     argsList = list(args)
     for i in range(n-1):
         for j in range (0, n-1):
-            if (argsList[j] > argsList[j+1]):
-                argsList[j], argsList[j+1] = argsList[j+1], argsList[j]
+            if (argsList[j] > argsList[j+1]):                               #J'indique que lorsque la première valeur est supérieur à la deuxième
+                argsList[j], argsList[j+1] = argsList[j+1], argsList[j]     #Alors la deuxième valeur sera prioritaire sur la première lors de l'affichage.
     return argsList
 
-def myDisplay(argsSort):
+def myDisplay(argsSort):                    #Sort me permet de modifier la liste.
     print(argsSort)
 myDisplay(mySort(5,8,34,2,20,90,3,7))
