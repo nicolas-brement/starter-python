7226c59e8c302f810118fc74941f117938bd0cf5
nicolas-brement <nicolas.brement@laplateforme.io>
2022-10-20 10:38:25 +0200

    Ajout des job

--- job21.py
+++ job21.py
@@ -1,12 +1,18 @@
-def mySort(*args):
-    n = len(args)
-    argsList = list(args)
-    for i in range(n-1):
-        for j in range (0, n-1):
-            if (argsList[j] > argsList[j+1]):                               #J'indique que lorsque la première valeur est supérieur à la deuxième
-                argsList[j], argsList[j+1] = argsList[j+1], argsList[j]     #Alors la deuxième valeur sera prioritaire sur la première lors de l'affichage.
-    return argsList
+def myFunction(*args):                      #Je créer "myFunction" suivi de mon paramètre.
+    args = list(args)                       #Je créer une première liste nommé "args".     
+    r=[]                                    #Je créer une deuxième liste nommé "r".
 
-def myDisplay(argsSort):                    #Sort me permet de modifier la liste.
-    print(argsSort)
-myDisplay(mySort(5,8,34,2,20,90,3,7))
+    while args:                             #Je créer une boucle while afin de pouvoir déplacer des valeurs d'un fichier à un autre par ordre croissant.
+        mini=args[0]
+        for x in args:                      
+            if x < mini:
+                mini=x
+        args.remove(mini)                   #.remove permet de supprimer la première valeur.
+        r.append(mini)                      #.append permet d'ajouter un élément à la fin de la liste.
+    return r
+
+
+def myDisplay(te):                          #Je créer une deuxième fonction "myDisplay(te)""
+    print(te)                               #Je demande à imprimer ma fonction
+myDisplay(myFunction(17,7,91,12,1,13,4,12,18)) #J'exécute mes deux fonctions.
+
